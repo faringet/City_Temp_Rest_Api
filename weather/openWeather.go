7dc66d1a0a169c841ac6e1b2c77e90557c5fc6be
faringet <mik.davydov14@gmail.com>
2022-12-25 17:19:15 +0300

    ReturnMap and city.list.json added

---
+++ weather/openWeather.go
@@ -0,0 +1,42 @@
+package weather
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Location struct {
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	State   string `json:"state"`
+	Country string `json:"country"`
+	Coord   struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	} `json:"coord"`
+}
+
+func ReturnMap() map[int]string {
+
+	// Read the JSON file
+	data, err := ioutil.ReadFile("city.list.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Unmarshal the JSON data
+	var locations []Location
+	err = json.Unmarshal(data, &locations)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Create a map to store the values
+	locationMap := make(map[int]string)
+	for _, location := range locations {
+		locationMap[location.ID] = location.Name
+	}
+
+	return locationMap
+}
